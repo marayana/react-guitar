219e54cc9743ff0992d5d1e8660884659700a2a2
jsdisco <mar18yyzz@gmail.com>
2020-12-18 10:15:44 +0100

    5-final-fretboard

--- src/Components/FretMarkers.js
+++ src/Components/FretMarkers.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { fbSize, fretPositions, stringPositions } from '../utils/fretboardValues.js';
 import { playNote, isMuted } from '../utils/sounds.js';
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Header(){
     return (
         <header><h1>React Guitar</h1></header>
